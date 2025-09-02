a11e965f714921a8cdf17ee455b28ad37a6e2094
unknown <Balungochares@gmail.com>
2025-09-02 20:57:02 +0800

    diana bossing

---
+++ resources/views/biographies/diana.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Diana Sofia L. Sta. Ana">
+
+
+    <iframe
+        src="https://chuchumiee.github.io/autobiography/"
+        width="100%"
+        height="600px"
+        style="border: none;">
+    </iframe>
+
+    <a href="{{ url('/') }}" class="back-btn">Back</a>
+</x-layout>
