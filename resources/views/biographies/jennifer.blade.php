a233f778cbd093896f7b958085fdebd510a02f64
unknown <Balungochares@gmail.com>
2025-09-02 20:17:55 +0800

    jennifer

---
+++ resources/views/biographies/jennifer.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Jennifer S. Fajardo">
+
+
+    <iframe
+        src="https://yennfajardo.github.io/profile/"
+        width="100%"
+        height="600px"
+        style="border: none;">
+    </iframe>
+
+    <a href="{{ url('/') }}" class="back-btn">Back</a>
+</x-layout>
