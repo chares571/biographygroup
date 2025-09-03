479fb22a83eca51ea6152a5e181ee8c3ed8e28f0
xxciaraxx <ciaralaranang3@gmail.com>
2025-09-03 13:55:31 +0800

    added ciara.blade.php

---
+++ resources/views/biographies/ciara.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Ciara Wilsen C. Laranang">
+    <iframe
+        src="https://xxciaraxx.github.io/biography/"
+        width="100%"
+        height="600px"
+        style="border: none;">
+    </iframe>
+
+    <a href="{{ url('/') }}" class="back-btn">Back</a>
+</x-layout>
